ab1016219b73fc90d0226bc765bcc110f93a38c8
codersuresh <sd807141@gmail.com>
2023-08-09 13:16:30 +0545

    remove unused import

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React from 'react'
-import Header from '@/components/Header'
 
 const Chat = () => {
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Header from '@/components/Header'
 import LoginForm from '@/components/LoginForm'
 
 const Home = () => {
