b6ed084bc9352c9e53989c7def088c0c35023710
codersuresh <sd807141@gmail.com>
2023-08-09 11:29:22 +0545

    refactor: separate header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
+import Header from '@/components/Header'
 
 const Home = () => {
   return (
-    <header className='px-10 py-5 shadow'>
-      <h1
-        className='font-bold text-xl text-slate-800'>
-        CHAT<span className='text-blue-600'>APP</span>
-      </h1>
-    </header>
+    <Header />
   )
 }
 
