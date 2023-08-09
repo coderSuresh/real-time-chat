eb8b078ccb10c17971d9b76e7fed908e3226c4b4
codersuresh <sd807141@gmail.com>
2023-08-09 13:13:28 +0545

    feat: remove header and main to use shared from layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,15 @@ const Home = () => {
 
   return (
     <>
-      <Header />
-      <main className='flex mt-10 flex-col items-center justify-center flex-1 px-20 text-center'>
-        <h1 className='text-3xl font-bold text-slate-800'>
-          Welcome to <span className='text-blue-600'>ChatApp!</span>
-        </h1>
-        <p className='mt-2 text-slate-600'>
-          A simple chat app built with Next.js, Tailwind CSS and SocketIO
-        </p>
+      <h1 className='text-3xl font-bold text-slate-800'>
+        Welcome to <span className='text-blue-600'>ChatApp!</span>
+      </h1>
+      <p className='mt-2 text-slate-600'>
+        A simple chat app built with Next.js, Tailwind CSS and SocketIO
+      </p>
 
-        <LoginForm />
+      <LoginForm />
 
-      </main>
     </>
   )
 }
