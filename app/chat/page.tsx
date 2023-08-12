71b4323f32d9604b8021ff4509f7d6561704a188
codersuresh <sd807141@gmail.com>
2023-08-12 12:27:38 +0545

    chat layout v1

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -24,6 +24,16 @@ const Chat = () => {
   return (
     <>
       <Header username={username} />
+
+      <main>
+        <div className='bg-white m-5 rounded md:p-10 p-5 h-[calc(100vh-200px)]'>
+          here will be chat
+        </div>
+        <div className='flex m-5 bg-white border rounded'>
+          <textarea rows={2} placeholder='Type your message' className='border-none outline-none resize-none border-gray-300 p-2 w-full' />
+          <button type='submit' className='bg-slate-800 p-2 text-white font-medium'>Send</button>
+        </div>
+     </main>
     </>
   )
 }
