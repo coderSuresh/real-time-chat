9e6f1f2446189d0e6329779fac83a4d3f6bbc3c5
codersuresh <sd807141@gmail.com>
2023-08-16 12:40:43 +0545

    define port separately

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 
 const io = new Server(server, {
@@ -21,6 +23,6 @@ io.on('connection', (socket) => {
     // Handle socket events here
 });
 
-server.listen(5000, () => {
-    console.log('Server is running on port 5000');
+server.listen(PORT, () => {
+    console.log(`Server: http://localhost:${PORT}`);
 });
