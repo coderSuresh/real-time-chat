4b39dc1f4a61f43283a277295fe0830b4878b8b1
codersuresh <sd807141@gmail.com>
2023-08-16 12:37:02 +0545

    remove comments

--- server/server.js
+++ server/server.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const http = require('http');
-const cors = require('cors'); // Import the cors package
+const cors = require('cors');
 const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 
-// Use the cors middleware for Express
 app.use(cors());
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000', // Replace with your frontend URL
-        methods: ['GET', 'POST'], // Specify the allowed HTTP methods
-        credentials: true, // Allow credentials (cookies, etc.)
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+        credentials: true,
     },
 });
 
