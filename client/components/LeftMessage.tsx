90de01afe30006c9933ccc17ac0c333bd02d8389
codersuresh <sd807141@gmail.com>
2023-08-17 22:33:37 +0545

    add some margin above the message chip

--- client/components/LeftMessage.tsx
+++ client/components/LeftMessage.tsx
@@ -8,14 +8,14 @@ type Props = {
 
 const LeftMessage = ({ username, message }: Props) => {
     return (
-        <div className="left_message self-start max-w-[60%]">
+        <div className="left_message mt-3 self-start max-w-[60%]">
             <div className='flex gap-3 items-end'>
                 <div>
                     {CircularAvatar(username)}
                 </div>
                 <div>
                     <p className='text-slate-500 px-3 text-sm'>{username}</p>
-                    <p className="bg-slate-800 text-slate-50 font-medium max-w-full break-all rounded-3xl px-3 py-2 mt-1">
+                    <p className="bg-slate-800 text-slate-50 w-fit font-medium max-w-full break-all rounded-3xl px-3 py-2 mt-1">
                         {message}
                     </p>
                 </div>
--- client/components/RightMessage.tsx
+++ client/components/RightMessage.tsx
@@ -8,14 +8,14 @@ type Props = {
 
 const RightMessage = ({ username, message }: Props) => {
     return (
-        <div className="right_message self-end max-w-[60%]">
+        <div className="right_message mt-3 self-end max-w-[60%]">
             <div className='flex flex-row-reverse gap-3 items-end'>
                 <div>
                     {CircularAvatar(username)}
                 </div>
                 <div className='flex flex-col'>
                     <p className='text-slate-500 px-3 text-sm self-end'>{username}</p>
-                    <p className="bg-slate-100 text-slate-800 font-medium rounded-3xl break-all px-3 py-2 mt-1">
+                    <p className="bg-slate-100 text-right max-w-fit self-end text-slate-800 font-medium rounded-3xl break-all px-3 py-2 mt-1">
                         { message }
                     </p>
                 </div>
