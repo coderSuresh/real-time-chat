03bb8ce06cd4e0454f7e8c1fb8d78ba5d416c8b3
codersuresh <sd807141@gmail.com>
2023-10-25 14:17:39 +0545

    remove duplicate code and remove new user joined msg for now

--- client/components/MessageCard.tsx
+++ client/components/MessageCard.tsx
@@ -1,52 +1,32 @@
 import React from 'react'
-import LeftMessage from './LeftMessage'
 import RightMessage from './RightMessage'
+import LeftMessage from './LeftMessage'
 
 const MessageCard = ({ myUsername, messages, newUserJoinedMessage }: Props) => {
-
     const [messageComponents, setMessageComponents] = React.useState<any>([])
 
     React.useEffect(() => {
         renderMessage()
-
-        if (newUserJoinedMessage) {
-            setMessageComponents((prevMessages: Message) => [
-                ...prevMessages,
-                <div className="text-center text-gray-500 text-sm" key={newUserJoinedMessage}>
-                    {newUserJoinedMessage}
-                </div>
-            ])
-        }
     }, [messages, newUserJoinedMessage])
 
-    // remove duplicate messages
-    React.useEffect(() => {
-        setMessageComponents((prevMessages: Message) => {
-            let newMessages = prevMessages.filter((message: any, i: number) => {
-                return prevMessages.findIndex((m: any) => m.key === message.key) === i
-            })
-            return newMessages
-        })
-    }, [messages])
-
     const renderMessage = () => {
+        const newMessageComponents = [] as React.ReactNode[]
 
         messages.forEach((message, i) => {
-
             if (!message.username || !message.message) return
 
             if (message.username === myUsername) {
-                setMessageComponents((prevMessages: Message) => [
-                    ...prevMessages,
+                newMessageComponents.push(
                     <RightMessage key={message.username + i} username={message.username} message={message.message} />
-                ])
+                )
             } else {
-                setMessageComponents((prevMessages: Message) => [
-                    ...prevMessages,
+                newMessageComponents.push(
                     <LeftMessage key={message.username + i} username={message.username} message={message.message} />
-                ])
+                )
             }
         })
+
+        setMessageComponents(newMessageComponents)
     }
 
     return <>{messageComponents}</>
